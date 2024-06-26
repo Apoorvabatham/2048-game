b8fab0a32d4fda27ad6516e088edd7386e5c2754
apoorva batham <apba00001@uni-saarland.de>
2024-06-26 03:39:05 +0200

    give me some ray

--- src/ttfe/Myimplementation.java
+++ src/ttfe/Myimplementation.java
@@ -9,6 +9,7 @@ public class Myimplementation implements SimulatorInterface{
     private int numMoves;
     private int [][] board;
     private Random random;
+    private int numPieces;
 
     public Myimplementation (int width, int height, Random random){
         if (width< 2|| height<2 || random == null){
@@ -18,7 +19,8 @@ public class Myimplementation implements SimulatorInterface{
         this.height = height;
         this.points = 0;
         this.numMoves = 0;
-        this.board = new int[width][height];
+        this.numPieces =0;
+        this.board = new int[height][width];
         this.random= random;
         addPiece();
         addPiece();
@@ -30,13 +32,14 @@ public class Myimplementation implements SimulatorInterface{
             throw new IllegalStateException("Board is full");
         }
         int x, y;
+
         do {
             x= random.nextInt(width);
             y= random.nextInt(height);
             } while (board[y][x] != 0);
         
-        board [y][x]= random.nextBoolean() ? 2 : 4; // 10% chance of 4, 90% chance of 2
-
+        board [y][x]= random.nextDouble() <0.9 ? 2 : 4; // 10% chance of 4, 90% chance of 2
+            numPieces++;
     }
 
     @Override
@@ -56,20 +59,12 @@ public class Myimplementation implements SimulatorInterface{
 
     @Override
     public int getNumPieces() {
-       int num = 0;
-       for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            if (board[i][j] != 0) {
-                num++;
-                }
-            }
-        }
-        return num ;
+        return numPieces ;
     }
 
     @Override
     public int getPieceAt(int x, int y) {
-        if (x <0 || y< 0 || x >= getBoardWidth() || y >= getBoardWidth()){
+        if (x <0 || y< 0 || x >= width || y >= height){
             throw new IllegalArgumentException("Coordinates out of dimensions");
         }
          return board[y][x];
@@ -83,9 +78,9 @@ public class Myimplementation implements SimulatorInterface{
     @Override
     public boolean isMovePossible() {
        if (isSpaceLeft()) return true;
-       for (int i = 0; i < width; i++) {
-        for (int j = 0; j< height; j++){
-            if ((i < width -1 && board [i][j]== board [i+1][j]) || (j< height-1 && board[i][j]== board [i][j+1]) ){
+       for (int i = 0; i < height; i++) {
+        for (int j = 0; j< width; j++){
+            if ((i < height -1 && board [i][j]== board [i+1][j]) || (j< width-1 && board[i][j]== board [i][j+1]) || (board[i][j]== 0) ){
                 return true;
             }
         }
@@ -102,7 +97,7 @@ public class Myimplementation implements SimulatorInterface{
         case NORTH:
         for (int i = 0; i < width; i++) {
             for (int j = 1; j < height; j++) {
-                if (board[i][j] != 0 && (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j])) {
+                if (board[j][i] != 0 && (board[j-1][i] == 0 || board[j-1][i] == board[j][i])) {
                     return true;
                 }
             }
@@ -111,8 +106,8 @@ public class Myimplementation implements SimulatorInterface{
     
         case SOUTH:
         for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (board[i][j] != 0 && (board[i][j + 1] == 0 || board[i][j +1] == board[i][j])) {
+            for (int j = height -2; j >= 0; j--) {
+                if (board[j][i] != 0 && (board[j+1][i] == 0 || board[j+1][i] == board[j][i])) {
                     return true;
                 }
             }
@@ -120,9 +115,9 @@ public class Myimplementation implements SimulatorInterface{
         break;
 
         case EAST:
-        for (int i = 0; i < width; i++) {
+        for (int i = width -2; i >= 0; i--) {
             for (int j = 0; j < height; j++) {
-                if (board[i][j] != 0 && (board[i+1][j] == 0 || board[i+1][j] == board[i][j])) {
+                if (board[j][i] != 0 && (board[j][i+1] == 0 || board[j][i+1] == board[j][i])) {
                     return true;
                 }
             }
@@ -130,14 +125,17 @@ public class Myimplementation implements SimulatorInterface{
         break;  
             
         case WEST:
-        for (int i = 0; i < width; i++) {
-            for (int j = 1; j < height; j++) {
-                if (board[i][j] != 0 && (board[i-1][j] == 0 || board[i-1][j] == board[i][j])) {
+        for (int i = 1; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (board[j][i] != 0 && (board[j][i-1] == 0 || board[j][i-1] == board[j][i])) {
                     return true;
                 }
             }
         }
-        break;    
+        break; 
+        
+        default :
+                    return false;
     }
     return false;
     }
@@ -155,26 +153,27 @@ public class Myimplementation implements SimulatorInterface{
     @Override
     public boolean performMove(MoveDirection direction) {
     if (direction == null) throw new IllegalArgumentException("Invalid direction 'null'")  ;
-    if (isMovePossible()) return false;
+    if (!(isMovePossible())) return false;
 
     boolean done = false;
     switch (direction) {
         case NORTH :
         for (int x = 0; x < width; x++) {
             for (int y = 1; y < height; y++) {
-                if (board[x][y] != 0) {
+                if (board[y][x] != 0) {
                  int currentY = y;
-                    while (currentY > 0 && board[x][currentY - 1] == 0) {
-                        board[x][currentY - 1] = board[x][currentY];
-                        board[x][currentY] = 0;
+                    while (currentY > 0 && board[currentY - 1][x] == 0) {
+                        board[currentY - 1][x] = board[currentY][x];
+                        board[currentY][x] = 0;
                         currentY--;
                         done = true;
                     }
-                    if (currentY > 0 && board[x][currentY - 1] == board[x][currentY]) {
-                        board[x][currentY - 1] *= 2;
-                        points += board[x][currentY - 1];
-                        board[x][currentY] = 0;
+                    if (currentY > 0 && board[currentY - 1][x] == board[currentY][x]) {
+                        board[currentY - 1][x] *= 2;
+                        points += board[currentY - 1][x];
+                        board[currentY][x] = 0;
                         done = true;
+                        numPieces --;
                     }
                 }
             }
@@ -184,19 +183,20 @@ public class Myimplementation implements SimulatorInterface{
         case SOUTH:
         for (int x = 0; x < width; x++) {
             for (int y = height - 2; y >= 0; y--) {
-                if (board[x][y] != 0) {
+                if (board[y][x] != 0) {
                 int currentY = y;
-                    while (currentY < height - 1 && board[x][currentY + 1] == 0) {
-                        board[x][currentY + 1] = board[x][currentY];
-                        board[x][currentY] = 0;
+                    while (currentY < height - 1 && board[currentY + 1][x] == 0) {
+                        board[currentY + 1][x] = board[currentY][x];
+                        board[currentY][x] = 0;
                         currentY++;
                         done = true;
                     }
-                    if (currentY < height - 1 && board[x][currentY + 1] == board[x][currentY]) {
-                        board[x][currentY + 1] *= 2;
-                        points += board[x][currentY + 1];
-                        board[x][currentY] = 0;
+                    if (currentY < height - 1 && board[currentY + 1][x] == board[currentY][x]) {
+                        board[currentY + 1][x] *= 2;
+                        points += board[currentY + 1][x];
+                        board[currentY][x] = 0;
                         done = true;
+                        numPieces --;
                     }
                 }
             }
@@ -206,19 +206,20 @@ public class Myimplementation implements SimulatorInterface{
         case EAST:
         for (int y = 0; y < height; y++) {
             for (int x = width - 2; x >= 0; x--) {
-                if (board[x][y] != 0) {
+                if (board[y][x] != 0) {
                  int currentX = x;
-                    while (currentX < width - 1 && board[currentX + 1][y] == 0) {
-                        board[currentX + 1][y] = board[currentX][y];
-                        board[currentX][y] = 0;
+                    while (currentX < width - 1 && board[y][currentX + 1] == 0) {
+                        board[y][currentX + 1] = board [y][currentX];
+                        board[y][currentX] = 0;
                         currentX++;
                         done = true;
                     }
-                    if (currentX < width - 1 && board[currentX + 1][y] == board[currentX][y]) {
-                        board[currentX + 1][y] *= 2;
-                        points += board[currentX + 1][y];
-                        board[currentX][y] = 0;
+                    if (currentX < width - 1 && board [y][currentX + 1] == board [y][currentX]) {
+                        board [y][currentX + 1] *= 2;
+                        points += board [y][currentX + 1];
+                        board [y] [currentX]= 0;
                         done = true;
+                        numPieces --;
                     }
                 }
             }
@@ -228,19 +229,20 @@ public class Myimplementation implements SimulatorInterface{
         case WEST:
         for (int y = 0; y < height; y++) {
             for (int x = 1; x < width; x++) {
-                if (board[x][y] != 0) {
+                if (board[y][x] != 0) {
                 int currentX = x;
-                    while (currentX > 0 && board[currentX - 1][y] == 0) {
-                        board[currentX - 1][y] = board[currentX][y];
-                        board[currentX][y] = 0;
+                    while (currentX > 0 && board[y][currentX - 1] == 0) {
+                        board [y][currentX - 1] = board [y][currentX];
+                        board [y][currentX] = 0;
                         currentX--;
                         done = true;
                     }
-                    if (currentX > 0 && board[currentX - 1][y] == board[currentX][y]) {
-                        board[currentX - 1][y] *= 2;
-                        points += board[currentX - 1][y];
-                        board[currentX][y] = 0;
+                    if (currentX > 0 && board [y][currentX - 1] == board[y][currentX]) {
+                        board[y][currentX - 1] *= 2;
+                        points += board[y][currentX - 1];
+                        board[y][currentX] = 0;
                         done = true;
+                        numPieces --;
                     }
                 }
             }
@@ -249,8 +251,7 @@ public class Myimplementation implements SimulatorInterface{
         }
 
         if (done){
-            numMoves ++;
-            addPiece();
+        numMoves ++;
         }
 
         return done; 
@@ -266,8 +267,9 @@ public class Myimplementation implements SimulatorInterface{
         while (isMovePossible()){
             MoveDirection direction = player.getPlayerMove(this, ui);
             if (performMove(direction)){
-                ui.updateScreen(this);
+               addPiece();
             }
+            ui.updateScreen(this);
         }
         ui.showGameOverScreen(this);
     }
@@ -276,9 +278,12 @@ public class Myimplementation implements SimulatorInterface{
     public void setPieceAt(int x, int y, int piece) {
         if (x <0 || y< 0 || x >= getBoardWidth() || y >= getBoardHeight() || (piece % 2 != 0)|| piece < 0){
             throw new IllegalArgumentException("Invalid piece value or invalid board dimensions");
+            }else if (board[y][x] != 0 && piece == 0){
+                numPieces--;
+            }else if (board[y][x]== 0 && piece != 0 ){
+                numPieces++;
             }
-             else {
-               board [y][x]= piece;
-             }     
-    } 
+            
+    board [y][x]= piece;
+    }
 }
